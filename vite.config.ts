791c6c7ab6f98739737eb098b0c2161934f53978
Prabath Banuka <prabathbanukada@gmail.com>
2025-06-05 11:38:13 +0530

    Correct configurations

--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,12 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        fallback: path.resolve(__dirname, 'index.html'),
+      }
+    }
+  }
 }));
